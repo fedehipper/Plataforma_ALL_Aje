f56a2d67724f5fafac9267e863849b93680dc0ab
fedehipper <fedehipper@gmail.com>
2016-12-19 02:02:10 -0300

    agregado alfil

--- All_DREZ/src/tablero_grafico.c
+++ All_DREZ/src/tablero_grafico.c
@@ -7,7 +7,7 @@
 #define COLOR_LINEAS 42
 #define COLOR_CUADRADOS 4
 
-BITMAP *b_peon_blanco, *b_peon_negro;
+BITMAP *b_peon_blanco, *b_peon_negro, *b_torre_blanca, *b_torre_negra, *b_alfil_blanco, *b_alfil_negro;
 
 void draw_bordes(void) {
 	rect(screen, ESCALA, ESCALA, 650, 650, palette_color[COLOR_LINEAS]);
@@ -69,8 +69,8 @@ void tablero_en_blanco(char campo[LADO][LADO]) {
 	}
 }
 
+// piezas en minuscula --> blancas y mayuscula --> negras
 void inicializar_posicion_peones(char campo[LADO][LADO]) {
-	// piezas en minuscula --> blancas y mayuscula --> negras
 	int i;
 	for(i = 0 ; i < 8 ; i++) {
 		campo[1][i] = 'p';
@@ -80,8 +80,24 @@ void inicializar_posicion_peones(char campo[LADO][LADO]) {
 	}
 }
 
+void inicializar_posicion_alfiles(char campo[LADO][LADO]) {
+	campo[0][2] = 'a';
+	campo[0][5] = 'a';
+	campo[7][2] = 'A';
+	campo[7][5] = 'A';
+}
+
+void inicializar_posicion_torres(char campo[LADO][LADO]) {
+	campo[0][0] = 't';
+	campo[0][7] = 't';
+	campo[7][0] = 'T';
+	campo[7][7] = 'T';
+}
+
 void inicializar_posicion_piezas(char campo[LADO][LADO]) {
 	inicializar_posicion_peones(campo);
+	inicializar_posicion_torres(campo);
+	inicializar_posicion_alfiles(campo);
 }
 
 void draw_tablero(void) {
@@ -90,6 +106,50 @@ void draw_tablero(void) {
 	draw_coordenadas();
 	draw_cuadros_tablero();
 	draw_bordes();
+}
+
+void crear_torre_blanca(void) {
+	int i, j;
+	b_torre_blanca = create_bitmap(80, 80);
+	clear_bitmap(b_torre_blanca);
+	for(i = 0 ; i < LADO_PIEZA ; i++) {
+		for(j = 0 ; j < LADO_PIEZA ; j++) {
+			putpixel(b_torre_blanca, i, j, palette_color[torre_blanca[j][i]]);
+		}
+	}
+}
+
+void crear_alfil_blanco(void) {
+	int i, j;
+	b_alfil_blanco = create_bitmap(80, 80);
+	clear_bitmap(b_alfil_blanco);
+	for(i = 0 ; i < LADO_PIEZA ; i++) {
+		for(j = 0 ; j < LADO_PIEZA ; j++) {
+			putpixel(b_alfil_blanco, i, j, palette_color[alfil_blanco[j][i]]);
+		}
+	}
+}
+
+void crear_alfil_negro(void) {
+	int i, j;
+	b_alfil_negro = create_bitmap(80, 80);
+	clear_bitmap(b_alfil_negro);
+	for(i = 0 ; i < LADO_PIEZA ; i++) {
+		for(j = 0 ; j < LADO_PIEZA ; j++) {
+			putpixel(b_alfil_negro, i, j, palette_color[alfil_negro[j][i]]);
+		}
+	}
+}
+
+void crear_torre_negra(void) {
+	int i, j;
+	b_torre_negra = create_bitmap(80, 80);
+	clear_bitmap(b_torre_negra);
+	for(i = 0 ; i < LADO_PIEZA ; i++) {
+		for(j = 0 ; j < LADO_PIEZA ; j++) {
+			putpixel(b_torre_negra, i, j, palette_color[torre_negra[j][i]]);
+		}
+	}
 }
 
 void crear_peon_blanco(void) {
@@ -134,9 +194,57 @@ void draw_peon_blanco(char campo[LADO][LADO]) {
 	}
 }
 
+void draw_torre_blanca(char campo[LADO][LADO]) {
+	int i, j;
+	for(i = 0 ; i < LADO ; i++) {
+		for(j = 0 ; j < LADO ; j++) {
+			if(campo[i][j] == 't') {
+				draw_sprite(screen, b_torre_blanca, j * 80 + 30, i * 80 + 30);
+			}
+		}
+	}
+}
+
+void draw_torre_negra(char campo[LADO][LADO]) {
+	int i, j;
+	for(i = 0 ; i < LADO ; i++) {
+		for(j = 0 ; j < LADO ; j++) {
+			if(campo[i][j] == 'T') {
+				draw_sprite(screen, b_torre_negra, j * 80 + 30, i * 80 + 30);
+			}
+		}
+	}
+}
+
+void draw_alfil_blanco(char campo[LADO][LADO]) {
+	int i, j;
+	for(i = 0 ; i < LADO ; i++) {
+		for(j = 0 ; j < LADO ; j++) {
+			if(campo[i][j] == 'a') {
+				draw_sprite(screen, b_alfil_blanco, j * 80 + 30, i * 80 + 30);
+			}
+		}
+	}
+}
+
+void draw_alfil_negro(char campo[LADO][LADO]) {
+	int i, j;
+	for(i = 0 ; i < LADO ; i++) {
+		for(j = 0 ; j < LADO ; j++) {
+			if(campo[i][j] == 'A') {
+				draw_sprite(screen, b_alfil_negro, j * 80 + 30, i * 80 + 30);
+			}
+		}
+	}
+}
+
 void crear_piezas(void) {
+	crear_torre_blanca();
+	crear_torre_negra();
 	crear_peon_blanco();
 	crear_peon_negro();
+	crear_alfil_blanco();
+	crear_alfil_negro();
 }
 
 void re_draw(char campo[LADO][LADO]) {
@@ -144,6 +252,11 @@ void re_draw(char campo[LADO][LADO]) {
 	draw_tablero();
 	draw_peon_blanco(campo);
 	draw_peon_negro(campo);
+	draw_torre_blanca(campo);
+	draw_torre_negra(campo);
+	draw_alfil_blanco(campo);
+	draw_alfil_negro(campo);
+
 }
 
 

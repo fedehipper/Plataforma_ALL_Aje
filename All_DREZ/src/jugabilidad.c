18f2f88ffe30cf3337c546ecb4dac309af920023
fedehipper <fedehipper@gmail.com>
2016-12-22 07:23:50 -0300

    refactor

--- All_DREZ/src/jugabilidad.c
+++ All_DREZ/src/jugabilidad.c
@@ -16,56 +16,41 @@ bool misma_coordenada(int fila, int columna, int nueva_fila, int nueva_columna) 
 }
 
 void seleccionar(char campo[LADO][LADO]) {
-	int fila = 0, columna = 0, fila_origen = 0, fila_destino = 0, columna_origen = 0, columna_destino = 0;
-	int clic_origen = 0;
+	int fila = 0, columna = 0, fila_origen = 0, fila_destino = 0, columna_origen = 0, columna_destino = 0, clic_origen = 0, tecla = 0;
 	char pieza = ' ';
 
-	int tecla = 0;
 	while(tecla != KEY_A) {
 		rest(75);
-
-
 
 		if(clic_origen == 1) {
 			fila_origen = fila;
 			columna_origen = columna;
 		}
 
-		 if(clic_origen > 1 && !misma_coordenada(fila_origen, columna_origen, fila, columna)) {
-			 fila_destino = fila;
-			 columna_destino = columna;
-			 mover_pieza_a_destino(pieza, fila_origen, fila_destino, columna_origen, columna_destino, campo);
-			 draw_selector_cuadrado(fila_destino, columna_destino, campo);
-			 draw_cuadrado(fila_origen, columna_origen, campo);
-			 clic_origen = 0;
-			 re_draw(campo);
-			 rest(300);
-			 re_draw(campo);
-		 }
+		if(clic_origen > 1 && !misma_coordenada(fila_origen, columna_origen, fila, columna)) {
+			fila_destino = fila;
+			columna_destino = columna;
+			mover_pieza_a_destino(pieza, fila_origen, fila_destino, columna_origen, columna_destino, campo);
+			draw_selector_cuadrado(fila_destino, columna_destino, campo);
+			draw_cuadrado(fila_origen, columna_origen, campo);
+			clic_origen = 0;
+			re_draw(campo);
+			rest(300);
+			re_draw(campo);
+		}
 
-
-		 if (mouse_b & 1) {
-			 fila = (mouse_y - 11) / 80;
-			 columna = (mouse_x - 11) / 80;
-			 printf("%d %d\n" ,fila,columna);
-			 draw_selector_cuadrado(fila, columna, campo);
-			 pieza = campo[fila][columna];
-			 if(!misma_coordenada(fila_origen, columna_origen, fila, columna))
-				 clic_origen += 1;
-		 }
-
+		if (mouse_b & 1) {
+			fila = (mouse_y - 11) / 80;
+			columna = (mouse_x - 11) / 80;
+			printf("%d %d\n" ,fila,columna);
+			draw_selector_cuadrado(fila, columna, campo);
+			pieza = campo[fila][columna];
+			clic_origen += 1;
+		}
 
 		 // fila --> y
 		 // columna --> x
-
-
-
-
-
-		 if(keypressed()) tecla = readkey() >> 8;
+		if(keypressed()) tecla = readkey() >> 8;
 	}
-
-
-	// verificar la posicion y si clic, tomar el x e y y dividirlo por 10 y mostrar por pantalla luego de cliquear unicamente
 
 }

bbc68b247aae23bff2fb20dbc12a70eee711bb07
fedehipper <fedehipper@gmail.com>
2016-12-24 00:48:06 -0300

    terminado movimiento posible rey

--- All_DREZ/src/movimientos_rey.c
+++ All_DREZ/src/movimientos_rey.c
@@ -11,7 +11,6 @@ bool es_amigo_de_rey(char rey, char otro) {
 }
 
 bool movimiento_permitido_rey(int fila_origen, int columna_origen, int fila_destino, int columna_destino, char campo[LADO][LADO]) {
-
 	return ((fila_origen + 1 == fila_destino && columna_origen == columna_destino) ||
 		    (fila_origen - 1 == fila_destino && columna_origen == columna_destino) ||
 		    (columna_origen + 1 == columna_destino && fila_origen == fila_destino) ||
@@ -20,8 +19,5 @@ bool movimiento_permitido_rey(int fila_origen, int columna_origen, int fila_dest
 		    (fila_origen - 1 == fila_destino && columna_origen - 1 == columna_destino) ||
 		    (fila_origen + 1 == fila_destino && columna_origen - 1 == columna_destino) ||
 		    (fila_origen - 1 == fila_destino && columna_origen + 1 == columna_destino)) &&
-			!es_amigo_de_rey(campo[fila_origen][fila_destino], campo[fila_destino][columna_destino]);
-
+			!es_amigo_de_rey(campo[fila_origen][columna_origen], campo[fila_destino][columna_destino]);
 }
-
-

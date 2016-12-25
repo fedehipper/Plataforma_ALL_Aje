1b76fdfb20194c5fd1318cc964652cbcbeddcb55
fedehipper <fedehipper@gmail.com>
2016-12-25 04:06:14 -0300

    realizado turnos de jugadores

--- All_DREZ/src/jugabilidad.c
+++ All_DREZ/src/jugabilidad.c
@@ -17,7 +17,7 @@ void aplicar_movimiento(int fila_origen, int columna_origen, int fila_destino, i
 	campo[fila_origen][columna_origen] = ' ';
 }
 
-void mover_pieza_a_destino(char pieza, int fila_origen, int fila_destino, int columna_origen, int columna_destino, char campo[LADO][LADO]) {
+bool mover_pieza_a_destino(char pieza, int fila_origen, int fila_destino, int columna_origen, int columna_destino, char campo[LADO][LADO]) {
 	bool mover = false;
 
 	if(campo[fila_origen][columna_origen] != ' ' && columna_destino != 8 && fila_destino != 8 && columna_origen != 8 && fila_origen != 8) {
@@ -43,6 +43,7 @@ void mover_pieza_a_destino(char pieza, int fila_origen, int fila_destino, int co
 		}
 		if(mover) aplicar_movimiento(fila_origen, columna_origen, fila_destino, columna_destino, campo);
 	}
+	return mover;
 }
 
 bool misma_coordenada(int fila, int columna, int nueva_fila, int nueva_columna) {
@@ -62,45 +63,98 @@ bool es_pieza_blanca(int fila, int columna, char campo[LADO][LADO]) {
 	return pieza == 'w' || pieza == 'a' || pieza == 'r' || pieza == 't' || pieza == 'c' || pieza == 'p';
 }
 
+void siguiente_turno(bool *turno_blanca, bool *turno_negra) {
+	if(turno_blanca) {
+		*turno_blanca =false;
+		*turno_negra = true;
+	} else {
+		*turno_blanca = true;
+		*turno_negra = false;
+	}
+}
+
 void seleccionar(char campo[LADO][LADO]) {
-	int fila = 0, columna = 0, fila_origen = 0, fila_destino = 0, columna_origen = 0, columna_destino = 0, clic = 0, tecla = 0;
+	int fila = 0, columna = 0, fila_origen = 0, fila_destino = 0, columna_origen = 0, columna_destino = 0,
+		clic_blanca = 0, clic_negra = 0, tecla = 0;
 	char pieza = ' ';
+
+	bool turno_blanca = true;
 
 	while(tecla != KEY_ESC) {
 		rest(75);
 
-		if(pieza != '_') {
-			if(clic == 1) {
-				fila_origen = fila;
-				columna_origen = columna;
-			}
-
-			if(clic > 1 && !misma_coordenada(fila_origen, columna_origen, fila, columna)) {
-				fila_destino = fila;
-				columna_destino = columna;
-				mover_pieza_a_destino(pieza, fila_origen, fila_destino, columna_origen, columna_destino, campo);
-				draw_selector_cuadrado(fila_destino, columna_destino, campo);
-				draw_cuadrado(fila_origen, columna_origen, campo);
-				re_draw(campo);
-				clic = 0;
-			}
-		}
-
 		if(mouse_b & 1) {
 			fila = (mouse_y - 11) / 80;
 			columna = (mouse_x - 11) / 80;
-			if(hay_pieza(fila, columna, campo)) {
-				draw_selector_cuadrado(fila, columna, campo);
-			}
-			pieza = obtener_pieza(fila, columna, campo);
-			if(pieza != '_') {
-				clic += 1;
-			}
-		}
 
-		if(mouse_b & 2) {
-			re_draw(campo);
-			clic = 0;
+			if(turno_blanca) {
+				if(hay_pieza(fila, columna, campo) && es_pieza_blanca(fila, columna, campo)) {
+					draw_selector_cuadrado(fila, columna, campo);
+					if(clic_blanca == 0) {
+						fila_origen = fila;
+						columna_origen = columna;
+						clic_blanca = 1;
+					}
+				}
+			} else {
+				if(hay_pieza(fila, columna, campo) && !es_pieza_blanca(fila, columna, campo)) {
+					draw_selector_cuadrado(fila, columna, campo);
+					if(clic_negra == 0) {
+						fila_origen = fila;
+						columna_origen = columna;
+						clic_negra = 1;
+					}
+				}
+			}
+
+			if(turno_blanca) {
+
+				if(clic_blanca == 1 && (fila != fila_origen || columna != columna_origen)) {
+					fila_destino = fila;
+					columna_destino = columna;
+					clic_blanca = 2;
+				}
+			} else {
+				if(clic_negra == 1 && (fila != fila_origen || columna != columna_origen)) {
+					fila_destino = fila;
+					columna_destino = columna;
+					clic_negra = 2;
+				}
+
+			}
+
+			if(turno_blanca) {
+
+				if(clic_blanca == 2) {
+					if(mover_pieza_a_destino(pieza, fila_origen, fila_destino, columna_origen, columna_destino, campo)) {
+						turno_blanca = false;
+					} else {
+						turno_blanca = true;
+					}
+					draw_selector_cuadrado(fila_destino, columna_destino, campo);
+					draw_cuadrado(fila_origen, columna_origen, campo);
+					re_draw(campo);
+					clic_blanca = 0;
+				}
+
+			} else {
+
+				if(clic_negra == 2) {
+					if(mover_pieza_a_destino(pieza, fila_origen, fila_destino, columna_origen, columna_destino, campo)) {
+						turno_blanca = true;
+					} else {
+						turno_blanca = false;
+					}
+					draw_selector_cuadrado(fila_destino, columna_destino, campo);
+					draw_cuadrado(fila_origen, columna_origen, campo);
+					re_draw(campo);
+					clic_negra = 0;
+				}
+
+			}
+
+
+
 		}
 
 		if(keypressed()) tecla = readkey() >> 8;

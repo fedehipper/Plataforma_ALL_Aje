b445e9dd6852e0878ad4e0a274752b1e8d387d56
fedehipper <fedehipper@gmail.com>
2016-12-27 00:16:49 -0300

    solucion errores, cuando es jaque, cualquier pieza puede salvar el jaque

--- All_DREZ/src/jugabilidad.c
+++ All_DREZ/src/jugabilidad.c
@@ -86,11 +86,15 @@ bool mover_pieza_a_destino(int fila_origen, int fila_destino, int columna_origen
 			}
 			break;
 		}
+
+		// * pieza esta en jaque, pero si el movimiento que hago lo saca de jaque ,
+
+
 		if(mover && !*pieza_esta_en_hacke) {
 			aplicar_movimiento(fila_origen, columna_origen, fila_destino, columna_destino, campo);
 			play_sample(sonido_mover, 200, 150, 1000, 0);
 		}
-		if(mover && *pieza_esta_en_hacke && (campo[fila_origen][columna_origen] == 'R' || campo[fila_origen][columna_origen] == 'r')) {
+		if(mover && *pieza_esta_en_hacke) {
 			aplicar_movimiento(fila_origen, columna_origen, fila_destino, columna_destino, campo);
 			play_sample(sonido_mover, 200, 150, 1000, 0);
 			*pieza_esta_en_hacke = false;
@@ -180,7 +184,7 @@ void seleccionar(char campo[LADO][LADO], SAMPLE * sonido_mover) {
 				if(condicion_blanca_seleccionar && !blanca_esta_en_jaque) {
 					seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
 				}
-				if(condicion_blanca_seleccionar && blanca_esta_en_jaque && campo[fila][columna] == 'r') {
+				if(condicion_blanca_seleccionar && blanca_esta_en_jaque) {
 					seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
 				}
 
@@ -213,11 +217,12 @@ void seleccionar(char campo[LADO][LADO], SAMPLE * sonido_mover) {
 
 				condicion_negra_seleccionar = hay_pieza(fila, columna, campo) && !es_pieza_blanca(fila, columna, campo);
 
-				if(condicion_negra_seleccionar && !negra_esta_en_jaque) {
-					seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
-				}
-				if(condicion_negra_seleccionar && negra_esta_en_jaque && campo[fila][columna] == 'R') {
-					seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
+				if(condicion_negra_seleccionar) {
+					if(!negra_esta_en_jaque) {
+						seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
+					} else {
+						seleccionar_origen(fila, columna, turno_blanca, &clic_blanca, &clic_negra, &fila_origen, &columna_origen, campo);
+					}
 				}
 
 				if(clic_negra == 1 && (fila != fila_origen || columna != columna_origen)) {

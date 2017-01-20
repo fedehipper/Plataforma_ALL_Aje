ad32642e233280e766c0ea190fa7655377ab3b86
fedehipper <federicohipper@gmail.com>
2017-01-20 17:16:17 -0300

    refactor

--- script.sh
+++ script.sh
@@ -2,4 +2,7 @@
 # -*- ENCODING: UTF-8 -*-
 echo
 	sudo apt-get install liballegro4.2-dev
+	sudo add-apt-repository ppa:webupd8team/haguichi
+	sudo apt-get update
+	sudo apt-get install haguichi
 exit
